0f3701e27fcd62f8c29fba500263953c15b62a7a
1526414 <1526414@uab.cat>
2024-06-23 00:11:55 +0200

    first commit

---
+++ SillyBallsSPT/Patches/ApplyDamageInfoPatch.cs
@@ -0,0 +1,36 @@
+﻿using Aki.Reflection.Patching;
+using EFT;
+using System.Reflection;
+using UnityEngine;
+
+namespace BloodParticles.Patches
+{
+    public class ApplyDamageInfoPatch : ModulePatch
+    {
+        protected override MethodBase GetTargetMethod()
+        {
+            return typeof(Player).GetMethod(nameof(Player.ApplyDamageInfo));
+        }
+
+        [PatchPrefix]
+        public static void Postfix(DamageInfo damageInfo, EBodyPart bodyPartType, EBodyPartColliderType colliderType, float absorbed)
+        {
+            /*if (damageInfo.DamageType != EDamageType.Bullet && damageInfo.DamageType != EDamageType.Sniper && damageInfo.DamageType != EDamageType.Explosion
+                && damageInfo.DamageType != EDamageType.GrenadeFragment && damageInfo.DamageType != EDamageType.Landmine &&
+                damageInfo.DamageType != EDamageType.Melee)
+                return; //maybe implement bleeds*/
+            if (damageInfo.DamageType == EDamageType.Explosion || damageInfo.DamageType == EDamageType.Landmine
+                || damageInfo.BlockedBy != "" || damageInfo.DeflectedBy != "")
+                return;
+            GameObject bloodParticles;
+            bool LongerParticles = Plugin.LongerParticles.Value;
+            if (bodyPartType == 0 && damageInfo.DamageType != EDamageType.Melee && damageInfo.DamageType != EDamageType.GrenadeFragment)
+                bloodParticles = Object.Instantiate(LongerParticles ? Plugin.bloodParticlesHead : Plugin.bloodParticlesHeadShort, damageInfo.HitPoint, Quaternion.identity);
+            else
+                bloodParticles = Object.Instantiate(LongerParticles ? Plugin.bloodParticlesBody : Plugin.bloodParticlesBodyShort, damageInfo.HitPoint, Quaternion.identity);
+            ParticleSystem particleSystem = bloodParticles.GetComponent<ParticleSystem>();
+            if (particleSystem != null)
+                particleSystem.Emit(1);
+        }
+    }
+}
---
+++ SillyBallsSPT/Plugin.cs
@@ -0,0 +1,47 @@
+﻿using BepInEx;
+using BepInEx.Configuration;
+using BepInEx.Logging;
+using BloodParticles.Patches;
+using System.IO;
+using System.Reflection;
+using UnityEngine;
+
+namespace BloodParticles
+{
+    [BepInPlugin("com.borkel.bloodparticles", "Borkel's Blood Particles", "1.0.0")]
+    public class Plugin : BaseUnityPlugin
+    {
+        public static ManualLogSource logger;
+        public static readonly string directory = Path.GetDirectoryName(Assembly.GetExecutingAssembly().Location);
+        //Silly Balls Config Settings
+        
+        public static GameObject bloodParticlesHead;
+        public static GameObject bloodParticlesHeadShort;
+        public static GameObject bloodParticlesBody;
+        public static GameObject bloodParticlesBodyShort;
+
+        public static ConfigEntry<bool> LongerParticles;
+        void Awake()
+        {
+            LongerParticles = Config.Bind("0.Particles", "Longer lasting particles", false, "Extends the duration of the particles");
+
+            string pluginDirectory = $"{directory}";//plugin folder
+            string bundlePath = $"{pluginDirectory}\\bloodparticles";
+            AssetBundle assetBundle = AssetBundle.LoadFromFile(bundlePath);
+            if (assetBundle == null)
+                Logger.LogMessage("assetbundle");
+            bloodParticlesHead = assetBundle.LoadAsset<GameObject>("BloodParticlesHead");
+            bloodParticlesHeadShort = assetBundle.LoadAsset<GameObject>("BloodParticlesHeadShort");
+            bloodParticlesBody = assetBundle.LoadAsset<GameObject>("BloodParticlesBody");
+            bloodParticlesBodyShort = assetBundle.LoadAsset<GameObject>("BloodParticlesBodyShort");
+            if(bloodParticlesBody == null || bloodParticlesBodyShort==null || bloodParticlesHead == null || bloodParticlesHeadShort==null)
+            {
+                Logger.LogError("error loading particles assets");
+                return;
+            }
+            new ApplyDamageInfoPatch().Enable();
+
+            Logger.LogInfo("Loaded BloodParticles");
+        }
+    }
+}
